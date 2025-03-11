5210905dec1fdfdf58dc7cb891db6f9752d411cc
Fabio Carneiro <fabioaacarneiro@gmail.com>
2025-03-11 14:20:45 -0300

    feat: add separate files for route handlers

--- handler/handler.go
+++ handler/handler.go
@@ -5,33 +5,3 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
-
-func CreateOpeningHandler(ctx *gin.Context)  {
-    ctx.JSON(http.StatusOK, gin.H{
-        "msg": "POST Opening",
-    })
-}
-
-func ShowOpeningHandler(ctx *gin.Context)  {
-    ctx.JSON(http.StatusOK, gin.H{
-        "msg": "GET Opening",
-    })
-}
-
-func DeleteOpeningHandler(ctx *gin.Context)  {
-    ctx.JSON(http.StatusOK, gin.H{
-        "msg": "DELETE Opening",
-    })
-}
-
-func UpdateOpeningHandler(ctx *gin.Context)  {
-    ctx.JSON(http.StatusOK, gin.H{
-        "msg": "PUT Opening",
-    })
-}
-
-func ListOpeningHandler(ctx *gin.Context)  {
-    ctx.JSON(http.StatusOK, gin.H{
-        "msg": "GET Opening",
-    })
-}
