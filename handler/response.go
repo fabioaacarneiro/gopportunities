f4b782b64f3a00c3733d9be870fdffe7e1d224ae
Fabio Carneiro <fabioaacarneiro@gmail.com>
2025-03-11 18:53:07 -0300

    add request and response helpers

---
+++ handler/response.go
@@ -0,0 +1,27 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func sendError(ctx *gin.Context, code int, msg string) {
+    // setting up header of request
+    ctx.Header("Content-Type", "application/json")
+
+    // setting up error body request
+    ctx.JSON(code, gin.H{
+        "message": msg,
+        "errorCode": code,
+    })
+}
+
+func sendSuccess(ctx *gin.Context, op string, data any) {
+    ctx.Header("Content-Type", "application/json")
+    ctx.JSON(http.StatusOK, gin.H{
+        "message": fmt.Sprintf("operation from handler: %s sendSuccess", op),
+        "data": data,
+    })
+}
