c455a6abf1d193d2de6e79e3176c106b9d9fc4f4
Fabio Carneiro <fabioaacarneiro@gmail.com>
2025-03-13 21:48:10 -0300

    fix: fix documentation description

--- handler/response.go
+++ handler/response.go
@@ -41,3 +41,13 @@ type DeleteOpeningResponse struct {
     Message string `json:"message"`
     Data schemas.OpeningResponse `json:"data"`
 }
+
+type ShowOpeningResponse struct {
+    Message string `json:"message"`
+    Data schemas.OpeningResponse `json:"data"`
+}
+
+type ListOpeningResponse struct {
+    Message string `json:"message"`
+    Data []schemas.OpeningResponse `json:"data"`
+}
