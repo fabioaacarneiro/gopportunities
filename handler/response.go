0530261479a127d6da81bacf5899362655c5d856
Fabio Carneiro <fabioaacarneiro@gmail.com>
2025-03-13 21:37:26 -0300

    docs: delete opening documentation

--- handler/response.go
+++ handler/response.go
@@ -36,3 +36,8 @@ type CreateOpeningResponse struct {
     Message string `json:"message"`
     Data schemas.OpeningResponse `json:"data"`
 }
+
+type DeleteOpeningResponse struct {
+    Message string `json:"message"`
+    Data schemas.OpeningResponse `json:"data"`
+}
